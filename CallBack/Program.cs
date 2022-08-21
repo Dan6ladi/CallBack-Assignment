bebcbb8e709e46747141a9b27bce2ab81bd9e01c
Dan6ladi <dan6ladi@gmail.com>
2022-08-21 21:21:41 +0100

    Add project files.

---
+++ CallBack/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CallBack
+{
+    public delegate void Download(int a);
+   
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            File file = new File();
+            Download download = new Download(file.CallBack);
+            file.FileDownload(download);
+           
+            Console.ReadLine();
+        }
+    }
+
+    public class File
+    {
+        public void FileDownload(Download download) 
+        {
+            for (int i = 0; i < 1000  ; i++) 
+            {
+                Console.WriteLine($"The file has been downloaded");
+
+            }
+        }
+        public void CallBack(int i) 
+        {
+            Console.WriteLine($"The File download has been completed {i} times");
+        }
+    }
+}
